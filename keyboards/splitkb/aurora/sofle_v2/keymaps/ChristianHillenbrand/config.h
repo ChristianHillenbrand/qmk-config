691736b9b38d1ccc3b94130d1cbbdf42428e8778
Christian Hillenbrand <cc-hillenbrand@gmx.de>
2024-05-25 16:12:49 +0200

    [chg] gui icon on oled screen

--- keyboards/splitkb/aurora/sofle_v2/keymaps/ChristianHillenbrand/config.h
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/ChristianHillenbrand/config.h
@@ -54,6 +54,9 @@
 #define ENABLE_RGB_MATRIX_STARLIGHT_DUAL_SAT
 #define ENABLE_RGB_MATRIX_RIVERFLOW
 
+#undef OLED_FONT_H
+#define OLED_FONT_H "keyboards/splitkb/aurora/sofle_v2/keymaps/ChristianHillenbrand/glcdfont.c"
+
 #define PERMISSIVE_HOLD_PER_KEY
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 
