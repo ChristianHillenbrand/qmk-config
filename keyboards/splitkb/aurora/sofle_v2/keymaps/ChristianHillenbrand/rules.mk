dbfc0559481814f543e172d3a93170d38bccccdd
Christian Hillenbrand <cc-hillenbrand@gmx.de>
2024-05-26 03:32:25 +0200

    [add] enable wpm

--- keyboards/splitkb/aurora/sofle_v2/keymaps/ChristianHillenbrand/rules.mk
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/ChristianHillenbrand/rules.mk
@@ -1,15 +1,10 @@
-CAPS_WORD_ENABLE = yes
-
 CONVERT_TO = liatris
-
-COMBO_ENABLE = yes
-
-ENCODER_ENABLE = yes
-
-KEY_OVERRIDE_ENABLE = yes
-
-RGB_MATRIX_ENABLE = yes
-
 SRC += features/layer_lock.c oled.c
 
+CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
+ENCODER_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
 TAP_DANCE_ENABLE = yes
+WPM_ENABLE = yes
