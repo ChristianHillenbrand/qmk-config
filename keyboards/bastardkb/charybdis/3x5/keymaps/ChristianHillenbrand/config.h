1619a077c5ddad0551a1695691a725ba0fa74916
Christian Hillenbrand <cc-hillenbrand@gmx.de>
2024-08-25 17:50:58 +0200

    [add] auto mouse layer for charbdis

--- keyboards/bastardkb/charybdis/3x5/keymaps/ChristianHillenbrand/config.h
+++ keyboards/bastardkb/charybdis/3x5/keymaps/ChristianHillenbrand/config.h
@@ -1,2 +1,6 @@
 #pragma once
 
+#include "layers.h"
+
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+#define AUTO_MOUSE_DEFAULT_LAYER L_MOUSE
--- keyboards/bastardkb/charybdis/3x5/keymaps/ChristianHillenbrand/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/ChristianHillenbrand/keymap.c
@@ -3,3 +3,11 @@
 #define X_LH KC_LCTL,
 
 #include "base.h"
+
+/*********
+ * MOUSE *
+ *********/
+
+void pointing_device_init_user(void) {
+  set_auto_mouse_enable(true);
+}
