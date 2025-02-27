ba305be7692d93d4d0f2271e54807dc339828630
Christian Hillenbrand <cc-hillenbrand@gmx.de>
2025-02-27 22:21:49 +0100

    [ref] minor changes

--- users/ChristianHillenbrand/layers.h
+++ users/ChristianHillenbrand/layers.h
@@ -4,7 +4,7 @@
 #define L_COLMK 1
 #define L_NAV   2
 #define L_FUN   3
-#define L_MEDIA 4
-#define L_NUM   5
-#define L_SYM   6
+#define L_NUM   4
+#define L_SYM   5
+#define L_MEDIA 6
 #define L_MOUSE 7
